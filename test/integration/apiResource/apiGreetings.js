196d2ac1a1c49bc94551cc778b925e71dd926b97
Damian Stanger <damian.stanger@gmail.com>
2017-05-20 13:02:10 +0100

    more refactorings of the greetings workflow test

--- test/integration/apiResource/apiGreetings.js
+++ test/integration/apiResource/apiGreetings.js
@@ -1,24 +1,37 @@
 module.exports = function (server, indexPayload) {
-  return {
-    "get": () => {
-      return server.inject({"url": indexPayload.greetings_url})
-        .then(getResponse => {
-          const greetingsPayload = JSON.parse(getResponse.payload);
-          greetingsPayload.statusCode = getResponse.statusCode;
 
-          greetingsPayload.post = payload => {
-            return server.inject({"method": "POST", "url": indexPayload.greetings_url, "payload": JSON.stringify(payload)})
-              .then(postResponse => {
-                const greetingPayload = JSON.parse(postResponse.payload);
-                greetingPayload.statusCode = postResponse.statusCode;
+  function post(payload) {
+    return server.inject({"method": "POST", "url": indexPayload.greetings_url, "payload": JSON.stringify(payload)})
+      .then(postResponse => {
+        const greetingPayload = JSON.parse(postResponse.payload);
+        greetingPayload.statusCode = postResponse.statusCode;
 
-                return greetingPayload;
-              });
-          };
+        // eslint-disable-next-line
+        greetingPayload.get = get;
+        greetingPayload.post = post;
 
-          return greetingsPayload;
-        });
+        return greetingPayload;
+      });
+  }
 
-    }
+  function get() {
+    return server.inject({"url": indexPayload.greetings_url})
+      .then(getResponse => {
+        const greetingsPayload = JSON.parse(getResponse.payload);
+        greetingsPayload.statusCode = getResponse.statusCode;
+
+        greetingsPayload.get = get;
+        greetingsPayload.post = post;
+
+        return greetingsPayload;
+      });
+  }
+
+
+  const greetingResource = {
+    get,
+    post
   };
+
+  return greetingResource;
 };
--- test/integration/apiResource/apiIndex.js
+++ test/integration/apiResource/apiIndex.js
@@ -1,13 +1,19 @@
 module.exports = function (server) {
-  return {
-    "get": () => {
-      return server.inject({"url": "/"})
-        .then(response => {
-          const indexPayload = JSON.parse(response.payload);
-          indexPayload.greetings = require("./apiGreetings")(server, indexPayload);
-          indexPayload.statusCode = response.statusCode;
-          return indexPayload;
-        });
-    }
+
+  function get() {
+    return server.inject({"url": "/"})
+      .then(response => {
+        const indexPayload = JSON.parse(response.payload);
+        indexPayload.greetings = require("./apiGreetings")(server, indexPayload);
+        indexPayload.statusCode = response.statusCode;
+        indexPayload.get = get;
+        return indexPayload;
+      });
+  }
+
+  const indexResource = {
+    "get": get
   };
+
+  return indexResource;
 };
