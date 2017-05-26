11ff2da5e9d79b1fbdb31527ad45fac487029f59
Damian Stanger <damian.stanger@gmail.com>
2017-05-26 15:40:00 +0100

    added test that will require all the files in the src folder so they are included in code coverage reports

---
+++ test/requireAllSourceForCodeCoverageReports.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-sync */
+/* eslint-disable no-console */
+
+const Lab = require("lab");
+const lab = exports.lab = Lab.script();
+const {describe, it} = lab;
+
+const fs = require("fs");
+const path = require("path");
+
+
+function isDirectory(fileName) {
+  const stats = fs.lstatSync(fileName);
+  return stats.isDirectory();
+}
+
+function requireAndSwallowErrors(fileName) {
+  try {
+    // console.log(`require(${fileName})`);
+    require(fileName);
+  } catch (err) {
+    console.log("[WARN]", `caught error running require(${fileName})`);
+  }
+}
+
+function requireAllTheThings(dirName, exclude) {
+  fs.readdir(dirName, (err, files) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+    files.map(fileName => path.join(dirName, fileName))
+      .filter(isDirectory)
+      .forEach(requireAllTheThings);
+
+    files.filter(fileName => fileName.endsWith(".js"))
+      .map(fileName => path.join("../", dirName, fileName))
+      .filter(fileName => fileName !== exclude)
+      .forEach(requireAndSwallowErrors);
+  });
+}
+
+
+describe("Load all source files for code coverage reports", () => {
+  it("require", done => {
+    const exclude = "../src/app.js";
+    requireAllTheThings("./src", exclude);
+    done();
+  });
+});
